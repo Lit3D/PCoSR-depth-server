8907ed6b56c2850b22157bb5cc0c761d61079057
Wisdman <wisdman@ajaw.it>
2020-11-21 15:40:18 +0500

    update

--- realsense_server_websocket.py
+++ realsense_server_websocket.py
@@ -25,22 +25,34 @@ def get_frame_in_background(device_sn):
   config.enable_device(device_sn)
   config.enable_stream(rs.stream.depth, 480, 270, rs.format.z16, 6)
   pipeline.start(config)
-  frames = pipeline.wait_for_frames()
-  frames.keep()
-  depth = frames.get_depth_frame()
-  depth.keep()
-  depthData = depth.as_frame().get_data()
-  depthMat = np.asanyarray(depthData)
-  print(depthMat)
+  while True:
+    global last_depth, lock
+    time_start = time()
+    frames = pipeline.wait_for_frames()
+    frames.keep()
+    depth = frames.get_depth_frame()
+    for filter in filters:
+      depth = filter.process(depth)
+    depth.keep()
+    depthData = depth.as_frame().get_data()
+    depthMat = np.asanyarray(depthData)
+    lock.acquire()
+    last_depth = depthMat
+    lock.release()
+    print ('Last frame processing took = %3i ms\r' %  (((time()-time_start) * 1000)) , end='' )
 
 print(connected_devices)
 Thread(target=get_frame_in_background, args=(connected_devices[0],)).start()
 
 async def websocket_reply(websocket, path):
-  received = await websocket.recv()
-  print(received)
+  time_start = time()
+  lock.acquire()
+  result = last_depth
+  lock.release()
+  await websocket.send(json.dumps(result))
+  print ('Retrieval Processing Time = %i ms' %  (((time()-time_start) * 1000)) )
 
-start_server = websockets.serve(websocket_reply, "localhost", 8787)
+start_server = websockets.serve(websocket_reply, port=8080)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
